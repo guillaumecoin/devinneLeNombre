e10dd18f97a8fa5a90db387df3220f9e531bcc5d
guillaume <guillaume.coin@espi.fr>
2022-12-27 20:40:31 +0100

    etat sans interface

--- main.py
+++ main.py
@@ -9,7 +9,25 @@ gui = tk.Tk()
 gui.geometry("500x500")
 gui.title('AkiNumber!')
 
-from tkinter import *
+
+canvas = tk.Canvas(gui, width=500, height=55, bg='black')
+canvas.create_text(50, 10, text= "Niveau 1: 0 à 10",fill="white",font=('Helvetica 9 bold'))
+canvas.create_text(50, 20, text= "Niveau 2: 0 à 50",fill="white",font=('Helvetica 9 bold'))
+canvas.create_text(53, 30, text= "Niveau 3: 0 à 100",fill="white",font=('Helvetica 9 bold'))
+canvas.create_text(53, 40, text= "Niveau 4: 0 à 500",fill="white",font=('Helvetica 9 bold'))
+canvas.create_text(57, 50, text= "Niveau 5: 0 à 1000",fill="white",font=('Helvetica 9 bold'))
+canvas.create_text(400, 10, text= "Niveau 6: 0 à 5000",fill="white",font=('Helvetica 9 bold'))
+canvas.create_text(403, 20, text= "Niveau 7: 0 à 10000",fill="white",font=('Helvetica 9 bold'))
+canvas.create_text(403, 30, text= "Niveau 8: 0 à 20000",fill="white",font=('Helvetica 9 bold'))
+canvas.create_text(403, 40, text= "Niveau 9: 0 à 40000",fill="white",font=('Helvetica 9 bold'))
+canvas.create_text(407, 50, text= "Niveau 10: 0 à 50000",fill="white",font=('Helvetica 9 bold'))
+canvas.pack(anchor=tk.NW, expand=True)
+Text_Resultat = tk.StringVar()
+Text_NbrCoups = tk.StringVar()
+NombreDeCoups =0
+
+
+# connection a la base de données + récupération du meilleur score par niveau
 def retourBdd (selected_niveau):
 
     try:
@@ -32,37 +50,23 @@ def retourBdd (selected_niveau):
             conn.close()
             return reponse
 
-response = retourBdd(1)
-fenetre = Tk()
-
-label = Label(fenetre, text=response)
-label.pack()
-
-fenetre.mainloop()
-
-canvas = tk.Canvas(gui, width=500, height=55, bg='black')
-canvas.create_text(50, 10, text= "Niveau 1: 0 à 10",fill="white",font=('Helvetica 9 bold'))
-canvas.create_text(50, 20, text= "Niveau 2: 0 à 50",fill="white",font=('Helvetica 9 bold'))
-canvas.create_text(53, 30, text= "Niveau 3: 0 à 100",fill="white",font=('Helvetica 9 bold'))
-canvas.create_text(53, 40, text= "Niveau 4: 0 à 500",fill="white",font=('Helvetica 9 bold'))
-canvas.create_text(57, 50, text= "Niveau 5: 0 à 1000",fill="white",font=('Helvetica 9 bold'))
-canvas.create_text(400, 10, text= "Niveau 6: 0 à 5000",fill="white",font=('Helvetica 9 bold'))
-canvas.create_text(403, 20, text= "Niveau 7: 0 à 10000",fill="white",font=('Helvetica 9 bold'))
-canvas.create_text(403, 30, text= "Niveau 8: 0 à 20000",fill="white",font=('Helvetica 9 bold'))
-canvas.create_text(403, 40, text= "Niveau 9: 0 à 40000",fill="white",font=('Helvetica 9 bold'))
-canvas.create_text(407, 50, text= "Niveau 10: 0 à 50000",fill="white",font=('Helvetica 9 bold'))
-canvas.pack(anchor=tk.NW, expand=True)
-Text_Resultat = tk.StringVar()
-Text_NbrCoups = tk.StringVar()
-NombreDeCoups =0
 def Start():
     global NombreDeCoups
     NombreDeCoups = NombreDeCoups + 1
     Nombre = int(MonNombre.get())
     if Nombre == nombreADeviner :
         Text_Resultat.set("Victoire!")
-        MonNom.pack(pady=20)
-        Send.pack()
+        reponse = retourBdd(selected_niveau)
+        if NombreDeCoups < reponse:
+
+            MonNom = tk.Entry(gui, width=15)
+            MonNom.pack_forget()
+            Send = tk.Button(gui, height=1, width=10, text="Envoyer", command= lambda : envoieBdd(NombreDeCoups, MonNom, selected_niveau))
+            Send.pack_forget()
+            Text_Resultat.set("Vous avez battu le record")
+
+
+
     else:
         if Nombre > nombreADeviner:
             Text_Resultat.set("Chercher un nombre plus petit")
@@ -98,28 +102,8 @@ def envoieBdd(NombreDeCoups, Nom, selected_niveau):
         conn.close()
 
 
-# connection a la base de données + récupération du meilleur score par niveau
-def retourBdd (NombreDeCoups,selected_niveau):
 
-    try:
-        conn = mysql.connector.connect(host="127.0.0.1",
-                                       user="root", password="",
-                                       database="python")
-        cursor = conn.cursor()
-        requestSelect = "SELECT meilleur_score FROM meilleurscore WHERE niveau_score={index:}"
-        cursor.execute(requestSelect.format(index=selected_niveau))
-        rows = cursor.fetchall()
-        for row in rows:
-            reponse = row[0]
 
-    except mysql.connector.errors.InterfaceError as e:
-        print("Error %d: %s" % (e.args[0], e.args[1]))
-        sys.exit(1)
-    finally:
-        # On ferme la connexion
-        if conn:
-            conn.close()
-            return reponse
 
 
 
@@ -129,10 +113,7 @@ def retourBdd (NombreDeCoups,selected_niveau):
 
 label_NbrCoups = tk.Label(gui, textvariable=Text_NbrCoups)
 label_NbrCoups.pack()
-MonNom = tk.Entry(gui, width=15)
-MonNom.pack_forget()
-Send = tk.Button(gui, height=1, width=10, text="Envoyer", command=envoieBdd)
-Send.pack_forget()
+
 label_Niveau = Label(gui, text="Selectionner le niveau")
 label_Niveau.pack()
 liste = Listbox(gui)
@@ -148,12 +129,28 @@ liste.insert(9, " 9")
 liste.insert(10, " 10")
 liste.pack()
 
+
+
+
+
 MonNombre = tk.Entry(gui, width=15)
 MonNombre.pack(pady=20)
+
+MonNom = tk.Entry(gui, width=30)
+MonNom.insert(0,"value")
+MonNom.pack()
+
+Send = tk.Button(gui, height=1, width=10, text="Envoyer",
+               command=lambda: envoieBdd(1, MonNom.get(), 1))
+Send.pack()
+
+
 btn = tk.Button(gui, height=1, width=10, text="Start", command=Start)
 btn.pack()
 label_Resultat = tk.Label(gui, textvariable=Text_Resultat)
 label_Resultat.pack()
 liste.bind('<<ListboxSelect>>', items_selected)
+
+
 
 gui.mainloop()
